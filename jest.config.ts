30c202be11f2509b5ef54a20072bb08cde7a2161
Rodrigo Adriano Meyer <rodrigo.meyer@totvs.com.br>
2023-01-20 12:07:18 -0300

    feat(project): melhorias

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,12 @@ const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
   preset: 'jest-preset-angular',
-  roots: ['<rootDir>/src/'],
   setupFilesAfterEnv: [
     '<rootDir>/node_modules/@angular-builders/jest/dist/jest-config/setup.js',
     '<rootDir>/setup-jest.ts'
   ],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
   testMatch: ['**/+(*.)+(spec).+(ts)'],
+  transformIgnorePatterns: ['^.+\\.js$'],
   collectCoverage: true,
   coverageReporters: ['html'],
   coverageDirectory: 'coverage/pokemon',
@@ -20,4 +17,9 @@ module.exports = {
   }),
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
